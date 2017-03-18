e84de73beb7198d38909c8ba2f839163f496d486
Harry Park <harry@harrypark.io>
2017-03-18 21:57:53 +1100

    user configurable height

--- fix.js
+++ fix.js
@@ -1,9 +1,20 @@
-'use strict';
-exports.decorateConfig = config => {
+exports.decorateConfig = (config) => {
+	const vsplit_fix = Object.assign({
+        vHeight: 34,
+    }, config.vsplit_fix);
+
 	return Object.assign({}, config, {
 		css: `
-		${config.css || ''}
-		.splitpane_panes_vertical .splitpane_pane+.splitpane_divider.splitpane_divider_vertical{height:calc(100% + 34px);margin-top:-34px}.splitpane_divider.splitpane_divider_horizontal+.splitpane_pane .splitpane_panes .splitpane_divider.splitpane_divider_vertical{height:100%;margin-top:0}
+			${config.css || ''}
+			.splitpane_panes_vertical .splitpane_pane + .splitpane_divider.splitpane_divider_vertical {
+				height: calc(100% + ${vsplit_fix.vHeight}px);
+				margin-top: -${vsplit_fix.vHeight}px;
+			}
+
+			.splitpane_divider.splitpane_divider_horizontal + .splitpane_pane .splitpane_panes .splitpane_divider.splitpane_divider_vertical {
+				height: 100%;
+				margin-top: 0;
+			}
 		`
 	});
 };
