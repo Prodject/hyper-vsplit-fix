a259c6250af88931ddacc91d1e844697acc881aa
Harry Park <harry@harrypark.io>
2017-03-18 21:26:27 +1100

    fix css ---> fix.js

--- fix.js
+++ fix.js
@@ -1,6 +1,9 @@
 'use strict';
 exports.decorateConfig = config => {
 	return Object.assign({}, config, {
-		css: `${config.css || ''}`
+		css: `
+		${config.css || ''}
+		.splitpane_panes_vertical .splitpane_pane+.splitpane_divider.splitpane_divider_vertical{height:calc(100% + 34px);margin-top:-34px}.splitpane_divider.splitpane_divider_horizontal+.splitpane_pane .splitpane_panes .splitpane_divider.splitpane_divider_vertical{height:100%;margin-top:0}
+		`
 	});
 };
